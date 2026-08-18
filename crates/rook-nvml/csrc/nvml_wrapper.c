da7f80a30b607ceed51c5357bbfb8757f50dbbd5
BeadiestStar64 <beadiestbench64@gmail.com>
2026-08-19 03:01:32 +0900

    feat: C言語側でVRAM情報を扱う機能を追加
    スカッシュマージで以下を編集
    
    commit 3c649fc48fef88f32d0ec8588caff5c1462780d1
    Author: BeadiestStar64 <beadiestbench64@gmail.com>
    Date:   Wed Aug 19 03:00:19 2026 +0900
    
        chore: `.clangd` のCompileFlagsを削除
    
    commit ac3cdb0c442b7e95369d2f8de7cffb67dfd3d9c6
    Author: BeadiestStar64 <beadiestbench64@gmail.com>
    Date:   Wed Aug 19 02:58:57 2026 +0900
    
        feat: VRAMの情報を取得する機能を追加

--- crates/rook-nvml/csrc/nvml_wrapper.c
+++ crates/rook-nvml/csrc/nvml_wrapper.c
@@ -1,4 +1,5 @@
 #include <nvml.h>
+#include <stddef.h>
 
 #include "nvml_wrapper.h"
 
@@ -15,4 +16,43 @@ int32_t rook_nvml_shutdown(void) {
 // GPUの総数を取得する
 int32_t rook_nvml_device_count(uint32_t* p_count) {
   return (int32_t)nvmlDeviceGetCount_v2(p_count);
+}
+
+// VRAM情報を取得する
+int32_t rook_nvml_device_memory(uint32_t index, rook_nvml_memory_t* p_memory) {
+  // ヌルポインターを許容しない
+  if (p_memory == NULL) {
+    return (int32_t)NVML_ERROR_INVALID_ARGUMENT;
+  }
+
+  // NVMLデバイスハンドルの格納先
+  nvmlDevice_t device;
+
+  // indexのGPUを参照するためのハンドルを取得
+  nvmlReturn_t result = nvmlDeviceGetHandleByIndex_v2(index, &device);
+
+  if (result != NVML_SUCCESS) {
+    return (int32_t)result;
+  }
+
+  // nvmlMemory_v2_tの各メンバをゼロ初期化
+  nvmlMemory_v2_t nvml_memory = {0};
+
+  // nvmMemory_v2_tの構造体バージョンを指定
+  nvml_memory.version = nvmlMemory_v2;
+
+  // VRAM情報を取得する
+  result = nvmlDeviceGetMemoryInfo_v2(device, &nvml_memory);
+
+  if (result != NVML_SUCCESS) {
+    return (int32_t)result;
+  }
+
+  // rook_nvml_memory構造体にデータを書き込む
+  p_memory->free = nvml_memory.free;
+  p_memory->reserved = nvml_memory.reserved;
+  p_memory->total = nvml_memory.total;
+  p_memory->used = nvml_memory.used;
+
+  return (int32_t)NVML_SUCCESS;
 }
