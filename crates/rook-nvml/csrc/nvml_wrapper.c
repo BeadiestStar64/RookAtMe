4054ad0547de63286ace9941d226e066c5bff541
BeadiestStar64 <beadiestbench64@gmail.com>
2026-08-18 23:55:04 +0900

    style: C言語のフォーマットを適用

--- crates/rook-nvml/csrc/nvml_wrapper.c
+++ crates/rook-nvml/csrc/nvml_wrapper.c
@@ -4,15 +4,15 @@
 
 // nvmlを初期化する
 int32_t rook_nvml_init(void) {
-	return (int32_t)nvmlInit_v2();
+  return (int32_t)nvmlInit_v2();
 }
 
 // nvmlをシャットダウンする
 int32_t rook_nvml_shutdown(void) {
-	return (int32_t)nvmlShutdown();
+  return (int32_t)nvmlShutdown();
 }
 
 // GPUの総数を取得する
 int32_t rook_nvml_device_count(uint32_t* p_count) {
-	return (int32_t)nvmlDeviceGetCount_v2(p_count);
+  return (int32_t)nvmlDeviceGetCount_v2(p_count);
 }
